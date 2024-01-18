e15a2fc05b8afcb60242065b820c999468382e73
Jackson Fellows <jtfellows@icloud.com>
2024-01-18 10:51:57 -0800

    Script to print to PDF.

---
+++ print.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+/Applications/Google\ Chrome.app/Contents/MacOS/Google\ Chrome --headless --print-to-pdf='/Users/jackson/Library/Mobile Documents/com~apple~CloudDocs/School/Jackson Fellows Resume.pdf' index.html
