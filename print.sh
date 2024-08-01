64ecb9ad42d91f24cb4e47e19b96b74b246d4522
Jackson Fellows <jtfellows@icloud.com>
2024-08-01 16:16:55 -0600

    Move resume to subpage, add basic index page

--- print.sh
+++ print.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-/Applications/Google\ Chrome.app/Contents/MacOS/Google\ Chrome --headless --print-to-pdf='/Users/jackson/Library/Mobile Documents/com~apple~CloudDocs/School/Jackson Fellows Resume.pdf' index.html
+/Applications/Google\ Chrome.app/Contents/MacOS/Google\ Chrome --headless --print-to-pdf='/Users/jackson/Library/Mobile Documents/com~apple~CloudDocs/School/Jackson Fellows Resume.pdf' resume.html
