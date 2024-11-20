10502e8f79d1128e303c7bc737349f6070a558fb
Jackson Fellows <jtfellows@icloud.com>
2024-11-20 13:31:20 -0500

    Update CV

--- print.sh
+++ print.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-/Applications/Google\ Chrome.app/Contents/MacOS/Google\ Chrome --headless --print-to-pdf='/Users/jackson/Library/Mobile Documents/com~apple~CloudDocs/School/Jackson Fellows Resume.pdf' resume.html
+/Applications/Google\ Chrome.app/Contents/MacOS/Google\ Chrome --headless --print-to-pdf=cv.pdf cv.html
